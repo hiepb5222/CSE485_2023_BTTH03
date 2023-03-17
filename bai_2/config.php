d704391e2e85b348313ea19dfa33da0c90a89754
Quoc Hiep <102863067+hiepb5222@users.noreply.github.com>
2023-03-17 22:47:52 +0700

    Update config.php

--- bai_2/config.php
+++ bai_2/config.php
@@ -2,7 +2,7 @@
 $db_host = 'localhost';
 $db_name = 'btth03';
 $db_user = 'root';
-$db_pass = 'Duong123';
+$db_pass = '';
 
 try {
     $db = new PDO("mysql:host=$db_host;dbname=$db_name;port=3307", $db_user, $db_pass);
